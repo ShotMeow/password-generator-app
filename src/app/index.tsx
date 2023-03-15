15db963f4dc955111172e7228f1ee9447a1b643e
ShotMeow <shotmeow@gmail.com>
2023-03-15 13:18:14 +0300

    Initialize: TailwindCSS

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import { Generator } from "../widgets/Generator/ui/Generator";
+
+export const App: FC = () => {
+  return (
+    <main>
+      <Generator />
+    </main>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./app/styles/globals.css";
+import { App } from "./app";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
